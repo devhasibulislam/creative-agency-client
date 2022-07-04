2e578d28e804d6aa7d61e196ace02643805b91a7
hasibulislam999 <hasib143sl@gmail.com>
2022-07-04 12:30:07 +0600

    initial directories and files created

---
+++ src/routes/Home/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import Header from '../Shared/Header';
+
+const Landing = () => {
+    return (
+        <section>
+            <Header />
+            This is Landing page.
+            <Footer />
+        </section>
+    );
+};
+
+export default Landing;
