93ca82bf962ce9d3bef892dc75ae4dc6c47ba36a
hasibulislam999 <hasib143sl@gmail.com>
2022-07-04 12:26:56 +0600

    header elsewhere done

---
+++ src/routes/Shared/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logos/logo.png';
+
+const Header = () => {
+    return (
+        <section className='container'>
+            <div id='header-section' className='d-flex justify-content-between'>
+                <div id='header-logo'>
+                    <Link to={'/home'}>
+                        <img
+                            src={logo}
+                            alt="creative agency"
+                        />
+                    </Link>
+                </div>
+                <div id='header-items' className='d-flex align-items-center'>
+                    <Link
+                        to='/admin'
+                        title='admin dashboard'
+                        className='header-item'
+                    >
+                        Dashboard
+                    </Link>
+                    <Link
+                        to='/customer'
+                        title='customer dashboard'
+                        className='header-item'
+                    >
+                        Dashboard
+                    </Link>
+                    <button id='authenticate-btn'>
+                        <Link
+                            to='/login'
+                        >
+                            Login
+                        </Link>
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Header;
