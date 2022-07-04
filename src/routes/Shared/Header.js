d89cfbd40f6e6cb047ff3c0e0c27ab867dad63bd
hasibulislam999 <hasib143sl@gmail.com>
2022-07-04 16:25:29 +0600

    background added on header

--- src/routes/Shared/Header.js
+++ src/routes/Shared/Header.js
@@ -4,38 +4,40 @@ import logo from '../../assets/logos/logo.png';
 
 const Header = () => {
     return (
-        <section className='container py-4'>
-            <div id='header-section' className='d-flex flex-lg-row flex-md-row flex-column justify-content-between align-items-center'>
-                <div id='header-logo'>
-                    <Link to={'/home'}>
-                        <img
-                            src={logo}
-                            alt="creative agency"
-                        />
-                    </Link>
-                </div>
-                <div id='header-items' className='d-flex align-items-center'>
-                    <Link
-                        to='/admin'
-                        title='admin dashboard'
-                        className='header-item'
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        to='/customer'
-                        title='customer dashboard'
-                        className='header-item'
-                    >
-                        Dashboard
-                    </Link>
-                    <button id='authenticate-btn' className='btn btn-dark'>
-                        <Link
-                            to='/login'
-                        >
-                            Login
+        <section id='header-section'>
+            <div className='container py-4'>
+                <div className='d-flex flex-lg-row flex-md-row flex-column justify-content-between align-items-center'>
+                    <div id='header-logo'>
+                        <Link to={'/home'}>
+                            <img
+                                src={logo}
+                                alt="creative agency"
+                            />
                         </Link>
-                    </button>
+                    </div>
+                    <div id='header-items' className='d-flex align-items-center'>
+                        <Link
+                            to='/admin'
+                            title='admin dashboard'
+                            className='header-item'
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            to='/customer'
+                            title='customer dashboard'
+                            className='header-item'
+                        >
+                            Dashboard
+                        </Link>
+                        <button id='authenticate-btn' className='btn btn-dark'>
+                            <Link
+                                to='/login'
+                            >
+                                Login
+                            </Link>
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>
