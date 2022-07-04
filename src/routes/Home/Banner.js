786f15d049f091f93c45230b4702d4ff21cba8e6
hasibulislam999 <hasib143sl@gmail.com>
2022-07-04 16:23:23 +0600

    bug fix with update

--- src/routes/Home/Banner.js
+++ src/routes/Home/Banner.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import frame from '../../assets/logos/Frame.png';
+import banner from '../../assets/banner-bg.png';
 
 const Banner = () => {
     return (
-        <section className='container'>
-            <div id='banner-section'>
-                <div
-                    className='row gx-lg-5 gy-5'
-                >
-                    <div className='col-lg-5 col-12'>
-                        <h2 id='banner-title'>
-                            Let's Grow Your <br />
-                            Brand To The <br />
-                            Next Level
-                        </h2>
-                        <p className='text-muted' id='banner-desc'>
-                            This is an inspiration series curated by Corebook° <br />
-                            with a few brilliant examples by designers aspiring to protect their brand design integrity with a brand book.
-                        </p>
-                        <button
-                            id='matched-btn'
-                            className='btn btn-dark'
-                        >
-                            Hire
-                        </button>
-                    </div>
+        <section
+            style={{
+                backgroundImage: `url(${banner})`
+            }}
+            id='landing-section'
+        >
+            <div className='container'>
+                <div id='banner-section'>
                     <div
-                        className='col-lg-7 col-12'
-                        style={{
-                            backgroundImage: `url(${frame})`
-                        }}
-                        id='banner-img'
+                        className='row gx-lg-5 gy-5'
                     >
-                        {/* <img
-                        src={frame}
-                        alt="frame"
-                    /> */}
+                        <div className='col-lg-5 col-12'>
+                            <h2 id='banner-title'>
+                                Let's Grow Your <br />
+                                Brand To The <br />
+                                Next Level
+                            </h2>
+                            <p className='text-muted' id='banner-desc'>
+                                This is an inspiration series curated by Corebook° <br />
+                                with a few brilliant examples by designers aspiring to protect their brand design integrity with a brand book.
+                            </p>
+                            <button
+                                id='matched-btn'
+                                className='btn btn-dark'
+                            >
+                                Hire
+                            </button>
+                        </div>
+                        <div
+                            className='col-lg-7 col-12'
+                            style={{
+                                backgroundImage: `url(${frame})`
+                            }}
+                            id='banner-img'
+                        >
+                        </div>
                     </div>
                 </div>
             </div>
