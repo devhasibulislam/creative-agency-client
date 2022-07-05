a850173ad4005e45d005b352e86d672614584629
hasibulislam999 <hasib143sl@gmail.com>
2022-07-05 11:51:53 +0600

    authorized routing included

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Admin from './routes/Admin/Admin';
 import Login from './routes/Authenticate/Login';
 import Register from './routes/Authenticate/Register';
+import RequireAuth from './routes/Authenticate/RequireAuth';
 import Customer from './routes/Customer/Customer';
 import Landing from './routes/Home/Landing';
 
@@ -12,8 +13,16 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path='/home' element={<Landing />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path='/customer' element={<Customer />} />
+        <Route path='/admin' element={
+          <RequireAuth>
+            <Admin />
+          </RequireAuth>
+        } />
+        <Route path='/customer' element={
+          <RequireAuth>
+            <Customer />
+          </RequireAuth>
+        } />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='*' element={<Landing />} />
