0ff4d20c02a9a362b4a179423f8706f6fd2e479d
hasibulislam999 <hasib143sl@gmail.com>
2022-07-05 20:58:56 +0600

    ui of admin panel done

--- src/routes/Admin/ServiceList.js
+++ src/routes/Admin/ServiceList.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 const ServiceList = () => {
     return (
-        <div>
-            This route shows service lists.
-        </div>
+        <section className='dashboard-section py-5'>
+            <div className='container'>
+                <div className='table-responsive'>
+                    <table class="table table-hover">
+                        <thead className='bg-white shadow rounded-pill'>
+                            <tr className=''>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email ID</th>
+                                <th scope="col">Service</th>
+                                <th
+                                    scope="col"
+                                    style={{ whiteSpace: 'nowrap' }}
+                                >Project Details</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td
+                                    style={{ whiteSpace: 'nowrap' }}
+                                >Sufi Ahmed Hamim</td>
+                                <td
+                                    style={{ whiteSpace: 'nowrap' }}
+                                >sufi01@gmail.com</td>
+                                <td
+                                    style={{ whiteSpace: 'nowrap' }}
+                                >Graphics designer</td>
+                                <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id cum mollitia sed dolorum illo aliquam soluta numquam laudantium beatae?</td>
+                                <td>Pending</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
     );
 };
 
