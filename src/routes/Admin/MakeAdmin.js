ad1db228b82274a46485807b77db904a960055af
hasibulislam999 <hasib143sl@gmail.com>
2022-07-05 20:32:38 +0600

    ui serving done

--- src/routes/Admin/MakeAdmin.js
+++ src/routes/Admin/MakeAdmin.js
@@ -2,9 +2,35 @@ import React from 'react';
 
 const MakeAdmin = () => {
     return (
-        <div>
-            This route can make a user or customer admin.
-        </div>
+        <section
+            id='order-section'
+            className='py-5 dashboard-section'
+        >
+            <div className='container'>
+                <div>
+                    <form>
+                        <div className='d-flex flex-lg-row flex-column justify-content-between'>
+                            <div className='w-75' id='make-admin-input'>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder='Existing email address...'
+                                    required
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                id='matched-btn'
+                                className='btn btn-success align-self-lg-center'
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
     );
 };
 
