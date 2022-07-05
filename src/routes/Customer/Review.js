d6c635aea23728a76a6b18b0dc730f460a97ea51
hasibulislam999 <hasib143sl@gmail.com>
2022-07-05 20:32:58 +0600

    ui serving done

--- src/routes/Customer/Review.js
+++ src/routes/Customer/Review.js
@@ -2,8 +2,49 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <section>
-            This is review route.
+        <section
+            id='order-section'
+            className='py-5 dashboard-section'
+        >
+            <div className='container'>
+                <div>
+                    <form>
+                        <div>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder='Your/Company name...'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder='Your email address...'
+                                required
+                            />
+                        </div>
+                        <div>
+                            <textarea
+                                name="message"
+                                className="description-box"
+                                placeholder='Product detail...'
+                                required
+                            ></textarea>
+                        </div>
+                        <button
+                            type="submit"
+                            id='matched-btn'
+                            className='btn btn-success'
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
         </section>
     );
 };
