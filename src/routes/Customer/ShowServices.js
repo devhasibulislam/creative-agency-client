10bcb72e09d3efe12cb4420c395459b82a83dd57
hasibulislam999 <hasib143sl@gmail.com>
2022-07-05 21:21:45 +0600

    customer ui done

--- src/routes/Customer/ShowServices.js
+++ src/routes/Customer/ShowServices.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import service1 from '../../assets/icons/service1.png';
+// import '../../App.css'
 
 const ShowServices = () => {
     return (
         <section>
-            This route shows services.
+            <div className='container'>
+                <div id='service-card'>
+                    <div className='p-5 dashboard-section'>
+                        <div className='d-flex justify-content-between mb-3'>
+                            <div>
+                                <img
+                                    src={service1}
+                                    alt="service1"
+                                    style={{
+                                        height: '74px',
+                                        width: '74px',
+                                        objectFit: 'cover'
+                                    }}
+                                />
+                            </div>
+                            <div>
+                                <span
+                                    class="badge rounded-pill text-bg-danger"
+                                    style={{fontWeight: '500'}}
+                                >
+                                    Pending
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                            <h4>
+                                Web & Mobile design
+                            </h4>
+                            <p className='text-muted'>
+                                We craft stunning and amazing web UI, using a well drrafted UX to fit your product.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
