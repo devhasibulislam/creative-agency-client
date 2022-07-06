081fe3d68a36f453f62d620c1e24397261bef22e
hasibulislam999 <hasib143sl@gmail.com>
2022-07-06 12:30:43 +0600

    approaching naming convension

--- src/routes/Admin/ServiceList.js
+++ src/routes/Admin/ServiceList.js
@@ -5,18 +5,19 @@ const ServiceList = () => {
         <section className='dashboard-section py-5'>
             <div className='container'>
                 <div className='table-responsive'>
-                    <table class="table table-hover">
+                    <table className="table table-hover">
                         <thead className='bg-white shadow rounded-pill'>
                             <tr className=''>
                                 <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Email ID</th>
-                                <th scope="col">Service</th>
+                                <th scope="col" className='ps-5'>Name</th>
+                                <th scope="col" className='ps-5'>Email ID</th>
+                                <th scope="col" className='ps-5'>Service</th>
                                 <th
                                     scope="col"
                                     style={{ whiteSpace: 'nowrap' }}
+                                    className='ps-5'
                                 >Project Details</th>
-                                <th scope="col">Status</th>
+                                <th scope="col" className='ps-5'>Status</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -24,15 +25,18 @@ const ServiceList = () => {
                                 <th scope="row">1</th>
                                 <td
                                     style={{ whiteSpace: 'nowrap' }}
+                                    className='ps-5'
                                 >Sufi Ahmed Hamim</td>
                                 <td
                                     style={{ whiteSpace: 'nowrap' }}
+                                    className='ps-5'
                                 >sufi01@gmail.com</td>
                                 <td
                                     style={{ whiteSpace: 'nowrap' }}
+                                    className='ps-5'
                                 >Graphics designer</td>
-                                <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id cum mollitia sed dolorum illo aliquam soluta numquam laudantium beatae?</td>
-                                <td>Pending</td>
+                                <td className='ps-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id cum mollitia sed dolorum illo aliquam soluta numquam laudantium beatae?</td>
+                                <td className='ps-5'>Pending</td>
                             </tr>
                         </tbody>
                     </table>
