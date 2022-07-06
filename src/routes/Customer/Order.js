c8798fd9e753853d7932430456c6a782d221eb9a
hasibulislam999 <hasib143sl@gmail.com>
2022-07-06 12:34:35 +0600

    order in progress of 40%

--- src/routes/Customer/Order.js
+++ src/routes/Customer/Order.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Order = () => {
+    const [user, loading] = useAuthState(auth);
+
+    const handleAddOrder = (event) => {
+        event.preventDefault();
+
+        const name = user?.displayName;
+        const email = user?.email;
+        const service = event.target.service.value;
+        const description = event.target.message.value;
+        const price = event.target.price.value;
+        const productInfo = { name, email, service, description, price };
+
+        console.log(productInfo);
+
+        event.target.reset();
+    };
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
         <section
             id='review-section'
@@ -8,14 +32,14 @@ const Order = () => {
         >
             <div className='container'>
                 <div>
-                    <form>
+                    <form onSubmit={handleAddOrder}>
                         <div>
                             <input
                                 type="text"
                                 name="name"
                                 id="name"
-                                placeholder='Your/Company name...'
-                                required
+                                value={user?.displayName}
+                                readOnly
                             />
                         </div>
                         <div>
@@ -23,14 +47,14 @@ const Order = () => {
                                 type="email"
                                 name="email"
                                 id="email"
-                                placeholder='Your email address...'
-                                required
+                                value={user?.email}
+                                readOnly
                             />
                         </div>
                         <div>
                             <input
                                 type="text"
-                                name="name"
+                                name="service"
                                 id="name"
                                 placeholder='Graphic design...'
                                 required
@@ -39,7 +63,7 @@ const Order = () => {
                         <div>
                             <textarea
                                 name="message"
-                                className="description-box"
+                                className="description"
                                 placeholder='Product detail...'
                                 required
                             ></textarea>
@@ -73,9 +97,9 @@ const Order = () => {
                                             color: '#009444'
                                         }}
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cloud-upload" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
-                                            <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cloud-upload" viewBox="0 0 16 16">
+                                            <path fillRule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
+                                            <path fillRule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
                                         </svg>
                                         <span className='ms-2'>upload project file</span>
                                     </p>
